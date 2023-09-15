200933cf3abaa793f45228eb1770563226957b62
engeneee <zhuldyzai.rashid@jihc.kz>
2023-09-15 21:20:02 +0600

    first commit

---
+++ sixth_lesson.dart
@@ -0,0 +1,23 @@
+// //Task 1
+// void main() {
+//   List<int> numbers = [1, 2, 3, 4, 5];
+//   List<int> tripledNumbers = numbers.map((number) => number * 3).toList();
+//   print(tripledNumbers);
+// }
+
+//Task 2
+void main() {
+  List<int> list1 = [1, 2, 3, 4];
+  List<int> list2 = [5, 6, 7, 8];
+  List<int> list3 = [9, 10, 11, 12];
+  
+  List<int> result = List.generate(
+    list1.length,
+    (index) => list1[index] + list2[index] + list3[index],
+  );
+
+  print(list1);
+  print(list2);
+  print(list3);
+  print(result);
+}
