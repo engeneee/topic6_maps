beb23e4eb6e130d92743cb5cc8f320c601183947
engeneee <zhuldyzai.rashid@jihc.kz>
2023-09-15 21:25:14 +0600

    first commit

--- sixth_lesson.dart
+++ sixth_lesson.dart
@@ -6,18 +6,18 @@
 // }
 
 //Task 2
-void main() {
-  List<int> list1 = [1, 2, 3, 4];
-  List<int> list2 = [5, 6, 7, 8];
-  List<int> list3 = [9, 10, 11, 12];
+// void main() {
+//   List<int> list1 = [1, 2, 3, 4];
+//   List<int> list2 = [5, 6, 7, 8];
+//   List<int> list3 = [9, 10, 11, 12];
   
-  List<int> result = List.generate(
-    list1.length,
-    (index) => list1[index] + list2[index] + list3[index],
-  );
+//   List<int> result = List.generate(
+//     list1.length,
+//     (index) => list1[index] + list2[index] + list3[index],
+//   );
 
-  print(list1);
-  print(list2);
-  print(list3);
-  print(result);
-}
+//   print(list1);
+//   print(list2);
+//   print(list3);
+//   print(result);
+// }
